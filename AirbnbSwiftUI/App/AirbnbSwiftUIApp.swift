1309ceb1d31f685f7da2634e917d281c0ff93219
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-11-05 21:45:40 +0530

    Initial project setup done

---
+++ AirbnbSwiftUI/App/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by Nalinda Wickramarathna on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbSwiftUI
+//
+//  Created by Nalinda Wickramarathna on 2023-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
